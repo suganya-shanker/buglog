b20b0e04eac88a355d82a1ab3d6a842b989bf8fb
Suganya <Suganya@FULL348-WIN.FULL.Creative>
2018-05-25 04:10:24 +0530

    First commit

---
+++ src/buglog/_BuglogServlet.java
@@ -0,0 +1,12 @@
+package buglog;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class _BuglogServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
